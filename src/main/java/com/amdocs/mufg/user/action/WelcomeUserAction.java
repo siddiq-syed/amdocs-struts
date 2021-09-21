8bbe8ff1bdaa6cada34d5a0b3cf49e6265463e9d
siddiq-syed <siddiq_sa@yahoo.com>
2021-09-21 11:28:23 -0400

    Add files via upload

---
+++ src/main/java/com/amdocs/mufg/user/action/WelcomeUserAction.java
@@ -0,0 +1,20 @@
+package com.amdocs.mufg.user.action;
+
+public class WelcomeUserAction {
+	private String username;
+	 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	// all struts logic here
+	public String execute() {
+
+		return "SUCCESS";
+
+	}
+}
